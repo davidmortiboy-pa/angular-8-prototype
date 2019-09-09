12ebf7e8072fb76bd5c2cff2c21568b57ad003d7
David Mortiboy <david.mortiboy@pamediagroup.com>
2019-09-09 11:05:45 +0100

    Generate SeriesList component using `ng g c`

--- src/app/series/series.module.ts
+++ src/app/series/series.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SeriesListComponent } from './series-list/series-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SeriesListComponent],
   imports: [
     CommonModule
   ]
